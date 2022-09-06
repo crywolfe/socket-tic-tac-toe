c9405335556c0bf975511c44c8ed67d5264fabfd
Gerry Wolfe <gerry@conduit.financial>
2022-09-05 19:57:18 -0700

    work up board.

--- src/board.ts
+++ src/board.ts
@@ -2,14 +2,12 @@ import {Symbol} from './players'
 
 export const board: string[] = ['.', '.', '.', '.', '.', '.', '.', '.', '.']
 
-export const visualBoard: string =  `
+export const getCurrentBoard = () => {
+  return `
   ${board[0]} ${board[1]} ${board[2]}
   ${board[3]} ${board[4]} ${board[5]}
   ${board[6]} ${board[7]} ${board[8]}
 `
-
-export const getCurrentBoard = () => {
-  return visualBoard
 }
 
 export const setCurrentBoard = (pos: number, player: Symbol) => {
